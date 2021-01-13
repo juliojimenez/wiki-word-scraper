f6a4cffe2a750a62f4a131d66a6d4b72255be57f
Julio Jimenez <julio@julioj.com>
2021-01-12 22:46:21 -0500

    word list test

--- tests.py
+++ tests.py
@@ -9,5 +9,13 @@ class TestWikiWordScraper(unittest.TestCase):
         result: str = get_wiki_text()
         self.assertIsInstance(result, str)
 
+    def test_word_list(self):
+        with open("testdata/test.html", "r") as f:
+            test_html: str = f.read()
+        result: list = word_list(test_html)
+        self.assertIsInstance(result, list)
+        self.assertTrue(len(result) > 0)
+
+
 if __name__ == "__main__":
     unittest.main()
