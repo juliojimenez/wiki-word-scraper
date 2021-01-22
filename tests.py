6953fd6791c83975239cf6b386384e341ba64c7d
Julio Jimenez <julio@julioj.com>
2021-01-21 23:09:17 -0500

    test

--- tests.py
+++ tests.py
@@ -1,17 +1,22 @@
+import io
 import unittest
-from wws import (
-    get_wiki_text,
-    word_list
-)
+from unittest.mock import MagicMock, patch
+from wws import add_to_main_file, get_wiki_text, word_list
+
 
 class TestWikiWordScraper(unittest.TestCase):
-    
+    def test_add_to_main_file(self):
+        with patch("builtins.open", create=True) as mock_open:
+            mock_open.return_value = MagicMock(spec=io.IOBase)
+            result: bool = add_to_main_file("/some/path", ["a", "b", "c"])
+        self.assertTrue(result)
+
     def test_get_wiki_text(self):
         result: str = get_wiki_text()
         self.assertIsInstance(result, str)
 
     def test_word_list(self):
-        print(f"[T] Testing with testdata/test.html")
+        print("[T] Testing with testdata/test.html")
         with open("testdata/test.html", "r") as f:
             test_html: str = f.read()
         result: list = word_list(test_html)
